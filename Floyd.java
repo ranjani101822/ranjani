179bc27d9fad98125a10074b2439b266e39fdb4d
ranjani101822 <ranjani101822@gmail.com>
2026-06-11 11:47:12 +0530

    floyd
    floyd

---
+++ Floyd.java
@@ -0,0 +1,110 @@
+// public class Floyd{
+//     public static void main(String[] args){
+//         int num=5;
+//         for(int i=1;i<=4;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(num);
+//                 num++;
+//             }
+//             System.out.println();
+//         }
+        
+//     }
+    
+// }
+
+
+
+// public class Floyd{
+//     public static void main(String[] args){
+//         int num=25;
+//         for(int i=1;i<=3;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(num);
+              
+
+                
+//             }
+//             System.out.println();
+//                num=num+2;
+           
+//         }
+        
+//     }
+    
+// }
+
+
+// public class Floyd{
+//     public static void main(String[] args){
+//          char ch='K';
+//         for(int i=1;i<=4;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(ch);
+              
+
+                
+//             }
+//             System.out.println();
+//             ch++;
+           
+//         }
+        
+//     }
+    
+// }
+
+
+
+
+// public class Floyd{
+//     public static void main(String[] args){
+//          char ch='A';
+//         for(int i=1;i<=5;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(ch);
+//               ch++;   
+//             }
+//             System.out.println();
+          
+           
+//         }
+        
+//     }
+    
+// }
+
+
+public class Floyd{
+    public static void main(String[] args){
+         
+        // for(int i=1;i<=5;i++){
+        //     char ch='b';
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(ch);
+        //        ch++;
+        //     }
+        //     System.out.println();
+            
+          
+           
+        // }
+
+        char ch='g';
+        for(int i=1;i<=4;i++){
+            
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+               
+            }
+            System.out.println();
+            ch+=2;
+            
+          
+           
+        }
+        
+        
+    }
+    
+}
