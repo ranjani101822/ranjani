9fad329dcf7d0e26a07fc8efce35bd4700320867
ranjani101822 <ranjani101822@gmail.com>
2026-06-11 11:46:02 +0530

    pattern
    pattern

---
+++ PatternEg.java
@@ -0,0 +1,102 @@
+// public class  PatternEg {
+//     public static void main(String[] args) {
+//         for(int i = 1; i <= 5; i++) {
+//             for(int j = 1; j <= 5; j++) {
+//                 if(i % 2 != 0)
+//                     System.out.print("*");
+//                 else
+//                     System.out.print("#");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+// public class  PatternEg {
+//     public static void main(String[] args) {
+//         for(int i = 1; i <= 5; i++) {
+//             for(int j = 1; j <= 5; j++) {
+//                 if(j % 2 != 0)
+//                     System.out.print("*");
+//                 else
+//                     System.out.print("#");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class PatternEg {
+//     public static void main(String[] args) {
+//         int count = 0;
+
+//         for(char ch = 'A'; ch <= 'Y'; ch++) {
+//             System.out.print(ch + " ");
+//             count++;
+
+//             if(count % 5 == 0)
+//                 System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class PatternEg {
+//     public static void main(String[] args) {
+//         int n = 5;
+
+//         for(int i = 1; i <= n; i++) {
+//             for(int j = 1; j <= n; j++) {
+//                 if(i == j || i + j == n + 1)
+//                     System.out.print("*");
+//                 else
+//                     System.out.print("#");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class PatternEg{
+//     public static void main(String[] args) {
+//         int n = 5;
+
+//         for(int i = 1; i <= n; i++) {
+//             for(int j = 1; j <= n; j++) {
+//                 if((i + j) % 2 == 0)
+//                     System.out.print("*");
+//                 else
+//                     System.out.print("#");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class Pattern {
+//     public static void main(String[] args) {
+//         int n = 4;
+
+//         for(int i = 1; i <= 7; i++) {
+//             for(int j = 1; j <= 7; j++) {
+
+//                 if(i == 1 || i == 7 || j == 1 || j == 7)
+//                     System.out.print("4 ");
+//                 else if(i == 2 || i == 6 || j == 2 || j == 6)
+//                     System.out.print("3 ");
+//                 else if(i == 3 || i == 5 || j == 3 || j == 5)
+//                     System.out.print("2 ");
+//                 else
+//                     System.out.print("1 ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
